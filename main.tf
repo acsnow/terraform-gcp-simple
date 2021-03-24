bbb32aa19beea435eba9c88d36c5045ae5c2384f
Craig Snow <craigs@ignw.io>
2021-03-24 11:42:19 -0700

    increased number of instances

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ variable "prefix" {
 }
 
 resource "google_compute_instance" "default" {
-    count = 1
+    count = 4
     name = "${var.prefix}-${count.index}"
     machine_type = "n1-standard-1"
  

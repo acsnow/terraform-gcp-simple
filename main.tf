4809c2e6d01d6cd569b971a3b8feff85374d7e44
Craig Snow <craigs@ignw.io>
2021-03-23 18:31:14 -0700

    added simple gcp build

---
+++ main.tf
@@ -0,0 +1,55 @@
+provider "google" {
+  #credentials = "../five9/secrets/packer-image-creator.json"
+  project      = "ignw-anthos-lab-pod1a"
+  region       = "us-west1"
+  zone 	       = "us-west1-a"
+}
+
+#data "google_compute_image" "ubuntu_image" {
+#  family  = "ubuntu-2004-lts"
+#  project = "ignw-anthos-lab-pod1a"
+#}
+
+variable "prefix" {
+  default = "csnow"
+}
+
+resource "google_compute_instance" "default" {
+    count = 2
+    name = "${var.prefix}-${count.index}"
+    machine_type = "f1-micro"
+ 
+    boot_disk {
+         initialize_params {
+             #image =  data.google_compute_image.ubuntu_image.self_link
+             image =  "ubuntu-2004-focal-v20210323"
+         }
+    }
+
+    metadata = {
+        ssh-keys = "csnow:${file("~/.ssh/id_rsa.pub")}"
+    }
+
+    network_interface {
+        network = "default"
+        access_config {
+            # nat_ip is here
+        }
+    }
+
+    #depends_on = ["google_compute_firewall.default"]
+}
+
+output "public_ip" {
+   value = google_compute_instance.default.*.network_interface.0.access_config.0.nat_ip
+}
+
+#resource "google_compute_firewall" "default" {
+#    name = "csnow-test-firewall"
+#    network = "default"
+#    
+#    allow {
+#        protocol = "tcp"
+#    }
+#}
+

d3c5a658c097126f063baf14809326b56f7c85af
Craig Snow <csnow@ignw.io>
2024-03-07 10:19:19 -0800

    added permission

---
+++ permission.tf
@@ -0,0 +1,32 @@
+##########################################################################################
+## Ensure default GCS Storage account can encrypt/decrypt on the cryto key(s):
+data "google_storage_project_service_account" "gcs_account" {
+  project = var.project_id
+}
+
+##########################################################################################
+## Ensure default Pub/Sub service account can encrypt/decrypt on the cryto key(s):
+resource "google_project_service_identity" "pubsub-sa" {
+  provider = google-beta
+  project  = var.project_id
+  service  = "pubsub.googleapis.com"
+}
+
+##########################################################################################
+## Ensure default Logging service account can encrypt/decrypt on the cryto key(s):
+resource "google_project_service_identity" "logging-sa" {
+  provider = google-beta
+  project  = var.project_id
+  service  = "logging.googleapis.com"
+}
+
+resource "google_kms_crypto_key_iam_binding" "cmek_bindings" {
+  crypto_key_id = module.cmek_key.self_link
+  role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
+  members = [
+    "serviceAccount:${data.google_storage_project_service_account.gcs_account.email_address}",
+    "serviceAccount:${google_project_service_identity.pubsub-sa.email}",
+    "serviceAccount:${google_project_service_identity.secretmanager-sa.email}",
+    "serviceAccount:${google_project_service_identity.logging-sa.email}"
+  ]
+}

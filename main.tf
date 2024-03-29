a3a7dbc98410a359239d2ae8c21cd5a4083baac3
Craig Snow <csnow@ignw.io>
2024-03-29 13:28:58 -0700

    Changed machine type

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "google" {
 resource "google_compute_instance" "default" {
     count = var.node_count
     name = "${var.prefix}-${count.index}"
-    machine_type = "n1-standard-1"
+    machine_type = "n1-standard-2"
  
     boot_disk {
          initialize_params {

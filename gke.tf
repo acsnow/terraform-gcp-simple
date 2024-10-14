f9d3ae73be18be9d95af3e36abbf369afa7d1b1c
Craig Snow <csnow@ignw.io>
2024-10-14 14:28:19 -0700

    added gke cluster

---
+++ gke.tf
@@ -0,0 +1,27 @@
+resource "google_container_cluster" "demo_openai_01" {
+  name     = "demo-wiz-01"
+  location = "us-west1-a"
+  deletion_protection = false
+  remove_default_node_pool = false
+
+  # We can define additional properties such as node pools, networking, etc.
+  initial_node_count = 1
+  node_config {
+    machine_type = "n2-standard-2"
+
+    # Configure the OAuth scopes to allow the nodes to access Google Cloud services
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+  }
+
+  network = "${var.vpc}"
+  subnetwork = "${var.subnet}"
+
+  # Enable some addons for the cluster
+  addons_config {
+    http_load_balancing        { disabled = false }
+    horizontal_pod_autoscaling { disabled = false }
+  }
+}
+

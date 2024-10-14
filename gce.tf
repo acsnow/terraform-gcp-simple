c7d2c1ca01e7d2273604625b51c2247ebaa41009
Craig Snow <csnow@ignw.io>
2024-10-14 15:17:09 -0700

    changed image type

--- gce.tf
+++ gce.tf
@@ -7,7 +7,8 @@ resource "google_compute_instance" "default" {
          initialize_params {
              #image =  "ubuntu-2004-focal-v20240830"
              #image =  "ubuntu-pro-1604-xenial-v20240924"
- 	      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+ 	     #image = "ubuntu-os-cloud/ubuntu-2004-lts"
+ 	     #image = "ubuntu-os-cloud/ubuntu-pro-1604-lts"
          }
     }
 

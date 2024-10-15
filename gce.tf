ad421c70644f149aa81806f1e88f71045138f746
Craig Snow <csnow@ignw.io>
2024-10-14 19:58:16 -0700

    change gke cluster size

--- gce.tf
+++ gce.tf
@@ -7,7 +7,8 @@ resource "google_compute_instance" "default" {
  
     boot_disk {
          initialize_params {
-             image =  "ubuntu-2004-focal-v20240830"
+             #image =  "ubuntu-2004-focal-v20240830"
+             image =  "ubuntu-pro-1804-bionic-v20240924"
              #image =  "ubuntu-pro-1604-xenial-v20240924"
          }
     }

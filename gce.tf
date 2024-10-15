faf1fc77f9d4adc3984534bbb0a6a5e4a7abf8d5
Craig Snow <csnow@ignw.io>
2024-10-15 16:11:32 -0700

    added service account

--- gce.tf
+++ gce.tf
@@ -29,7 +29,6 @@ resource "google_compute_instance" "default" {
     }
     service_account {
         # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-        email  = "1051112938655-compute@developer.gserviceaccount.com"
-        scopes = ["cloud-platform"]
+        email  = "tf-provisioner-dev@admin-services-279721.iam.gserviceaccount.com"
     }
 }

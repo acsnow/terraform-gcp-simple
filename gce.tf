975aedbb61d26b06ac3be7032675c0a4b3884330
Craig Snow <acsnow@gmail.com>
2025-12-03 13:00:52 -0700

    changed machine type

--- gce.tf
+++ gce.tf
@@ -8,8 +8,7 @@
 resource "google_compute_instance" "default" {
     count = var.node_count
     name = "${var.prefix}-${count.index}"
-    machine_type = "n1-standard-2"
-    #machine_type = "n1-standard-1"
+    machine_type = "n1-standard-1"
  
     boot_disk {
          initialize_params {
